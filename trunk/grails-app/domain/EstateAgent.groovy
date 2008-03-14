16b6aba01e7964d9963cc9de616abbb31c97a326
romain.bossut <romain.bossut@7c14548c-2747-0410-87fc-5f245b88402c>
2008-03-14 15:21:38 +0000

    - Good attributes in EstateAgent Class - Error fixed in Property Class

--- trunk/grails-app/domain/EstateAgent.groovy
+++ trunk/grails-app/domain/EstateAgent.groovy
@@ -1,12 +1,10 @@
 class EstateAgent {
-    
-    //id and version attributes are relative to the class management inside grails
-    //note that the id field could be also useful for the programmer
-    Long id
-    String agent
-    String phoneNumber
-    String password
-    Long version
+
+    String mySurname
+    String myForename
+    String myPassword
+    String myTelephoneNo
+
 
     //Note: Since the O.6 version of grails there is no need to specify any collection or Set for the hasMany
     //associations. relatesToMany used to require that but is now deprecated so only hasMany must be used
