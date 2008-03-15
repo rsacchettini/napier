7a8927040d5a9221da4bbc4d3a925efea3604e1c
romain.bossut <romain.bossut@7c14548c-2747-0410-87fc-5f245b88402c>
2008-03-15 15:18:13 +0000

    buyer, seller, and estateAgent now inherits from an User class.

--- grails-app/domain/Property.groovy
+++ grails-app/domain/Property.groovy
@@ -1,5 +1,5 @@
 class Property {
-    
+
     Seller isSoldBy
     EstateAgent isManagedBy
 
@@ -7,24 +7,24 @@ class Property {
     String myForename
     String myUsername
 
-    static belongsTo =  [isSoldBy:Seller]
-    
+    static belongsTo = [isSoldBy: Seller]
+
     //Note: Since the O.6 version of grails there is no need to specify any collection or Set for the hasMany
     //associations. relatesToMany used to require that but is now deprecated so only hasMany must be used
-    static hasMany = [interestedBuyers:Buyer, appointements:Appointement]
+    static hasMany = [interestedBuyers: Buyer, appointements: Appointement]
 
-     String toString() { return myForename+" "+mySurname }
+    String toString() {return myForename + " " + mySurname}
 
-      boolean equals(other) {
-          if(other?.is(this))return true
-          if(!(other instanceof Property)) return false
-          if(!id || !other?.id || id!=other?.id) return false
-          return true
-      }
+    boolean equals(other) {
+        if (other?.is(this)) return true
+        if (!(other instanceof Property)) return false
+        if (!id || !other?.id || id != other?.id) return false
+        return true
+    }
 
-      int hashCode() {
-          int hashCode = 0
-          hashCode = 29 * (hashCode + ( !id ? 0 : id ^ (id >>> 32)))
-      }
+    int hashCode() {
+        int hashCode = 0
+        hashCode = 29 * (hashCode + (!id ? 0 : id ^ (id >>> 32)))
+    }
 
 }
