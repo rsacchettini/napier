036e4d1c0c788c1a38eb40f9f22811bf72224927
romain.bossut <romain.bossut@7c14548c-2747-0410-87fc-5f245b88402c>
2008-03-15 15:18:13 +0000

    buyer, seller, and estateAgent now inherits from an User class.

--- trunk/grails-app/domain/Buyer.groovy
+++ trunk/grails-app/domain/Buyer.groovy
@@ -1,22 +1,21 @@
-class Buyer {
-    
+class Buyer extends User {
 
     //Note: Since the O.6 version of grails there is no need to specify any collection or Set for the hasMany
     //associations. relatesToMany used to require that but is now deprecated so only hasMany must be used
-    static hasMany = [availableProperties:Property, appointements:Appointement]
+    static hasMany = [availableProperties: Property, appointements: Appointement]
+    def belongsTo = Property
 
-    String toString() { "${this.class.name} :  $id" }
 
-      boolean equals(other) {
-          if(other?.is(this))return true
-          if(!(other instanceof Buyer)) return false
-          if(!id || !other?.id || id!=other?.id) return false
-          return true
-      }
+    boolean equals(other) {
+        if (other?.is(this)) return true
+        if (!(other instanceof Buyer)) return false
+        if (!id || !other?.id || id != other?.id) return false
+        return true
+    }
 
-      int hashCode() {
-          int hashCode = 0
-          hashCode = 29 * (hashCode + ( !id ? 0 : id ^ (id >>> 32)))
-      }
+    int hashCode() {
+        int hashCode = 0
+        hashCode = 29 * (hashCode + (!id ? 0 : id ^ (id >>> 32)))
+    }
 
 }
--- trunk/grails-app/domain/EstateAgent.groovy
+++ trunk/grails-app/domain/EstateAgent.groovy
@@ -1,27 +1,21 @@
-class EstateAgent {
-
-    String mySurname
-    String myForename
-    String myPassword
-    String myTelephoneNo
-
+class EstateAgent extends User {
 
     //Note: Since the O.6 version of grails there is no need to specify any collection or Set for the hasMany
     //associations. relatesToMany used to require that but is now deprecated so only hasMany must be used
-    static hasMany = [propertiesToManage:Property, appointementsToManage:Appointement]
+    static hasMany = [propertiesToManage: Property, appointementsToManage: Appointement]
 
-       String toString() { return myForename+" "+mySurname }
-       
+    String toString() {return myForename + " " + mySurname}
 
-      boolean equals(other) {
-          if(other?.is(this))return true
-          if(!(other instanceof EstateAgent)) return false
-          if(!id || !other?.id || id!=other?.id) return false
-          return true
-      }
 
-      int hashCode() {
-          int hashCode = 0
-          hashCode = 29 * (hashCode + ( !id ? 0 : id ^ (id >>> 32)))
-      }
+    boolean equals(other) {
+        if (other?.is(this)) return true
+        if (!(other instanceof EstateAgent)) return false
+        if (!id || !other?.id || id != other?.id) return false
+        return true
+    }
+
+    int hashCode() {
+        int hashCode = 0
+        hashCode = 29 * (hashCode + (!id ? 0 : id ^ (id >>> 32)))
+    }
 }
