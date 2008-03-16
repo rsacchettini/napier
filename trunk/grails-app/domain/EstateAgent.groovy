5d109dc141a212c375b68edfeb6c25eeeb49d045
romatou18 <romatou18@7c14548c-2747-0410-87fc-5f245b88402c>
2008-03-16 17:28:52 +0000

    Hallelujah : It works i have managed to generate all models and controllers classes ! Conflict resolved

--- trunk/grails-app/domain/EstateAgent.groovy
+++ trunk/grails-app/domain/EstateAgent.groovy
@@ -1,17 +1,75 @@
-class EstateAgent extends User {
+class EstateAgent {
 
- 
-    static hasMany = [propertiesToManage: Property, appointementsToManage: Appointment]
+    String mySurname
+    String myForename
+    String myUsername
+    String myPassword
+    String myTelephoneNo
 
-    boolean equals(other) {
-        if (other?.is(this)) return true
-        if (!(other instanceof EstateAgent)) return false
-        if (!id || !other?.id || id != other?.id) return false
-        return true
+    static constraints = 
+    {
+        mySurname(blank:false)
+        myForename(blank:false)
+        myUsername(blank:false, unique:true, length:5..15)
+        myPassword(blank:false, matches:/[\w\d]+/)
+        myTelephoneNo(blank:false)
     }
+    
 
-    int hashCode() {
-        int hashCode = 0
-        hashCode = 29 * (hashCode + (!id ? 0 : id ^ (id >>> 32)))
-    }
+    //Note: Since the O.6 version of grails there is no need to specify any collection or Set for the hasMany
+    //associations. relatesToMany used to require that but is now deprecated so only hasMany must be used
+    static hasMany = [propertiesToManage:Property, appointementsToManage:Appointment]
+
+       String toString() { "$myForename $mySurname" }
+
+      boolean equals(other) {
+          if(other?.is(this))return true
+          if(!(other instanceof EstateAgent)) return false
+          if(!id || !other?.id || id!=other?.id) return false
+          return true
+      }
+
+      int hashCode() {
+          int hashCode = 0
+          hashCode = 29 * (hashCode + ( !id ? 0 : id ^ (id >>> 32)))
+      }
+
+    /*
+
+    void listSellerProperty() {
+
+	}
+
+	void listInterestedBuyer() {
+
+	}
+
+	void search() {
+
+	}
+
+	void confirmSellerProperty() {
+
+	}
+
+	void refuseSellerProperty() {
+
+	}
+
+	void editInformation() {
+
+	}
+
+	void makeAppointment() {
+
+	}
+
+	void editAppointment() {
+
+	}
+
+	void removeProperty() {
+
+	}
+	*/
 }
--- trunk/grails-app/domain/Property.groovy
+++ trunk/grails-app/domain/Property.groovy
@@ -1,32 +1,53 @@
-class Property {
-    /* ---------- Attributes ---------- */
+class Property
+{
+    
     Seller isSoldBy
     EstateAgent isManagedBy
+
     String myReferenceNo
-	String myAvailableFrom
-	String myAddress
+	Date[] myAvailableFrom
+	String myaddress
 	String myPostCode
-	String myMap
-	double myMinPrice
-	double myMaxPrice
+	float myMinPrice
+	float myMaxPrice
 	String myDescription
 	int myBedroomNumber
-	String myPicture
+	byte[] myPicture
+
+	static constraints = {
+        myReferenceNo(blank:false, minLength:1, unique:true)
+        myAvailableFrom(minSize:2)
+        myaddress(blank:false, minLength:5)
+        myPostCode(blank:false, matches:/^(GIR 0AA)|([A-PR-UWYZ]((\d(\d|[A-HJKSTUW])?)|([A-HK-Y]\d(\d|[ABEHMNPRV-Y])?)) \d[ABD-HJLNP-UW-Z]{2})$/)
+        myMinPrice(min:1.0f)
+        myDescription(blank:false, maxLength:1000)
+        myBedroomNumber(min:1)
+    }
+
+
+    static belongsTo =  [Seller]
     
-    static belongsTo = [isSoldBy: Seller]
-    static hasMany = [interestedBuyers: Buyer, appointements: Appointment]
+    //Note: Since the O.6 version of grails there is no need to specify any collection or Set for the hasMany
+    //associations. relatesToMany used to require that but is now deprecated so only hasMany must be used
+    static hasMany = [interestedBuyers:Buyer, appointements:Appointment]
 
+     String toString() { "reference number $myReferenceNo" }
 
-    boolean equals(other) {
-        if (other?.is(this)) return true
-        if (!(other instanceof Property)) return false
-        if (!id || !other?.id || id != other?.id) return false
-        return true
-    }
+      boolean equals(other) {
+          if(other?.is(this))return true
+          if(!(other instanceof Property)) return false
+          if(!id || !other?.id || id!=other?.id) return false
+          return true
+      }
 
-    int hashCode() {
-        int hashCode = 0
-        hashCode = 29 * (hashCode + (!id ? 0 : id ^ (id >>> 32)))
-    }
+      int hashCode() {
+          int hashCode = 0
+          hashCode = 29 * (hashCode + ( !id ? 0 : id ^ (id >>> 32)))
+      }
+
+      float myMinPrice()
+      {
+          return this.myMinPrice
+      }
 
 }
