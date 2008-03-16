333da4d19814fd0ba4920540872c2ce50d971eb3
skhedim <skhedim@7c14548c-2747-0410-87fc-5f245b88402c>
2008-03-16 15:56:01 +0000

    I added the attribute myMap to the Property class. I added all the attributes of the Seller class and the Property class.

--- trunk/grails-app/domain/Seller.groovy
+++ trunk/grails-app/domain/Seller.groovy
@@ -1,7 +1,6 @@
 class Seller extends User {
     /* ---------- Attributes ---------- */
-
-    String address
+    String myAddress
 
     //Note: Since the O.6 version of grails there is no need to specify any collection or Set for the hasMany
     //associations. relatesToMany used to require that but is now deprecated so only hasMany must be used
@@ -18,6 +17,31 @@ class Seller extends User {
     int hashCode() {
         int hashCode = 0
         hashCode = 29 * (hashCode + (!id ? 0 : id ^ (id >>> 32)))
+    }
+    
+    /* ---------- Methods ---------- */
+    Seller (){}
+
+    public void addProperty() {
+
+
+    }
+
+	public void editProperty() {
+
+	}
+    public void removeProperty() {
+
+
+    }
+
+	public void listInterestedBuyers() {
+
+	}
+	
+	public void editPersonalDetails() {
+
+
     }
 
 }
--- trunk/grails-app/domain/User.groovy
+++ trunk/grails-app/domain/User.groovy
@@ -1,15 +1,23 @@
 class User {
-
-    String surname
-    String forename
-    String username
-    String password
-    String email
-    String telephoneNumber
+    /* ---------- Attributes ---------- */
+    String mySurname
+	String myForename
+	String myUsername
+	String myPassword
+	String myEmail
+	String myTelephoneNumber
 
     //This string will be returned by default in lists views when the selection of an User is required.
     String toString() {
         return myForename + " " + mySurname
     }
 
+    /* ---------- Methods ---------- */
+	public void logon() {
+
+	}
+
+	public void logout() {
+
+	}
 }
