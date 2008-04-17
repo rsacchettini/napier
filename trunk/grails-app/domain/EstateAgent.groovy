d4a4a796ec0d3b32d424fd0917a8cb1205f3116b
romatou18 <romatou18@7c14548c-2747-0410-87fc-5f245b88402c>
2008-04-17 17:02:38 +0000

    bug solving in user management

--- trunk/grails-app/domain/EstateAgent.groovy
+++ trunk/grails-app/domain/EstateAgent.groovy
@@ -1,4 +1,4 @@
-class EstateAgent extends User {
+class EstateAgent extends AuthUser {
 
     //Note: Since the O.6 version of grails there is no need to specify any collection or Set for the hasMany
     //associations. relatesToMany used to require that but is now deprecated so only hasMany must be used
